417c49882b2afa46990e5c9081474aa3cb412309
bavykin.grisha <bavykin.grisha@syk.fi>
2022-08-20 21:12:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        System.out.println("Example 1: " + Math.pow(3,a));
+        int A = 3;
+        int B = 2;
+        System.out.println("Example 2: " + Math.pow(A,B));
+
+    }
+}
